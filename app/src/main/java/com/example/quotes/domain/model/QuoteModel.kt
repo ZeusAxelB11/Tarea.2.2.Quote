335bb3c4b15f0a3e6758ecb32c7dafdd9b1817f2
Zeus Axel Bustos Alavez <zeusss.za@gmail.com>
2022-10-17 01:34:40 -0500

    update 17-10-22

--- app/src/main/java/com/example/quotes/domain/model/QuoteModel.kt
+++ app/src/main/java/com/example/quotes/domain/model/QuoteModel.kt
@@ -1,4 +1,3 @@
 package com.example.quotes.domain.model
 
-class QuoteModel (val quote: String, val author: String) {
-}
+data class QuoteModel(val quote: String, val author: String)
--- app/src/main/java/com/example/quotes/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/quotes/presentation/view/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.example.quotes.presentation.view
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.viewModels
@@ -58,11 +54,5 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
