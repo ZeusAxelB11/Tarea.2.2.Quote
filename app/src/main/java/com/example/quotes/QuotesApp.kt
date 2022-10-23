d2d27be99e146c43e5c5b141200626bb0baef03a
Zeus Axel Bustos Alavez <zeusss.za@gmail.com>
2022-10-23 18:25:41 -0500

    update 23-10-22

---
+++ app/src/main/java/com/example/quotes/QuotesApp.kt
@@ -0,0 +1,9 @@
+package com.example.quotes
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class QuotesApp: Application()
+
+annotation class HiltAndroidApp
--- app/src/main/java/com/example/quotes/presentation/viewmodel/QuoteViewModel.kt
+++ app/src/main/java/com/example/quotes/presentation/viewmodel/QuoteViewModel.kt
@@ -2,13 +2,16 @@ package com.example.quotes.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.quotes.data.QuoteProvider
-import com.example.quotes.domain.model.QuoteModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.itvo.quotes.domain.model.QuoteModel
+import edu.itvo.quotes.domain.usecase.GetQuoteRandomUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class QuoteViewModel : ViewModel() {
+/*class QuoteViewModel : ViewModel() {
 
     private val _quoteModel = MutableStateFlow(QuoteModel("",""))
     val quoteModel: StateFlow<QuoteModel> = _quoteModel
@@ -18,6 +21,22 @@ class QuoteViewModel : ViewModel() {
         viewModelScope.launch {
             val quote= QuoteProvider.random()
             _quoteModel.value = quote
+        }
+    }
+}*/
+@HiltViewModel
+class QuoteViewModel
+@Inject constructor(private val getQuoteRandomUseCase: GetQuoteRandomUseCase
+): ViewModel() {
+
+    private val quoteModelRandomMutableStateFlow = MutableStateFlow(QuoteModel(0,"",""))
+    val quoteModel: StateFlow<QuoteModel> = quoteModelRandomMutableStateFlow
+
+
+    fun randomQuote() {
+        viewModelScope.launch {
+            quoteModelRandomMutableStateFlow.value = getQuoteRandomUseCase.getQuoteRandom().first()
+            //_quoteModel.value = GetQuoteUseCase(quoteDAO).getQuote(1).first()
         }
     }
 }
